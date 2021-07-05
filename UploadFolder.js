162b87f13f9a9ef7606f364bf30cb7b9721807de
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2021-07-05 18:32:51 +0200

    Upload folder

--- UploadFolder.js
+++ UploadFolder.js
@@ -1,14 +1,25 @@
-UploadFileToNAV('C:/Users/Jesus/Documents/3Regs.txt');
+const testFolder = 'C:/Users/Jesus/Documents/Proyecto js/';
+const fs = require('fs');
 
-function UploadFileToNAV(FilePath)
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    const path = require('path');      
+    if (fs.lstatSync(path.resolve(testFolder, file)).isFile()) {      
+      UploadFileToNAV(testFolder,file);
+      console.log(file);
+    }  
+  });
+});
+function UploadFileToNAV(Folder,file)
 {
     FilePath = 'C:/Users/Jesus/Documents/3Regs.txt'
+    FilePath = Folder + file;
     var request = require("request");
     auth = require('./ApiKey.json');
     var fs = require('fs');
     content = fs.readFileSync(FilePath,{encoding:'utf8', flag:'r'});
     var transUrl = require('./BaseUri.json').URL + "/Sandbox/ODataV4/NAVBlobFileStorage_LoadFile?Company=CRONUS%20ES"
-    var data2 = {"fileType": "EDI-ORDER", "fileName":"ORDER2030.txt",
+    var data2 = {"fileType": "EDI-ORDER", "fileName":file,
         "content":Buffer.from(content).toString('base64')};
     request.post(
         {     
